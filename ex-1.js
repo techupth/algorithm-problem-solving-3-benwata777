a98166c6d7c864a170351a535217efdd2780a894
benwata777 <benwata777@gmail.com>
2024-07-24 14:49:47 +0700

    answer

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,17 @@
 function sortCustomerName(customers) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i = 0; i < customers.length; i++) {
+    for (let j = 0; j < customers.length; j++) {
+      if (customers[j] > customers[j + 1]) {
+        let temp = customers[j];
+        customers[j] = customers[j + 1];
+        customers[j + 1] = temp;
+      }
+    }
+  }
+  return customers;
 }
 
-// ตอบคำถามตรงนี้จ้า
+let customers = ["Somchai", "Arthit", "Suchada", "Napasorn", "Pimchanok"];
+console.log(sortCustomerName(customers));
+
+//big o เป็น On2 เพราะลูปสองรอบ
